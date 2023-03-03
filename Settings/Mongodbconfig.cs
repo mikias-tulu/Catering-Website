52b1669d4f4d18e599af6b9528bdec7f8601ad63
Miki T <miki.tulu@gmail.com>
2023-03-03 19:44:14 +0300

    Mongodb Configuration

---
+++ Settings/Mongodbconfig.cs
@@ -0,0 +1,13 @@
+﻿namespace adminautentication.Settings
+{
+    public class Mongodbconfig
+    {
+        public string Name { get; set; }
+
+        public string Host { get; set; }
+
+        public string Port { get; set; }
+
+        public string ConnectionString => $"mongodb://{Host}:{Port}";
+    }
+}
