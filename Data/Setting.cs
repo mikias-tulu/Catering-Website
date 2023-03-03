3eb7541e85705496882c30684f9671f51adc3ad3
Miki T <miki.tulu@gmail.com>
2023-03-03 20:20:12 +0300

    admin authentication Data initial commit

---
+++ Data/Setting.cs
@@ -0,0 +1,14 @@
+﻿namespace adminautentication.Data
+{
+    public class Setting
+    {
+        public const string MongoDB = "MongoDB";
+
+        public string ConnectionString { get; set; }
+
+        public string Database { get; set; }
+
+
+
+    }
+}
---
+++ Data/SweetcateringDBContext.cs
@@ -0,0 +1,65 @@
+﻿using adminautentication.Interface;
+using adminautentication.Models;
+using MongoDB.Driver;
+
+namespace adminautentication.Data
+{
+    public class SweetcateringDBContext : ISweetcatering
+    {
+
+        public MongoClient _mongoClient => new MongoClient("mongodb://127.0.0.1:27017");
+
+        public IMongoDatabase _database => _mongoClient.GetDatabase("Sweetcatering");
+        public IMongoCollection<CustomerEntity> _collection => _database.GetCollection<CustomerEntity>("customers");
+
+
+        public IEnumerable<CustomerEntity> GetAllCustomers()
+        {
+            return _collection.Find(a => true).ToList();
+        }
+
+        public CustomerEntity GetCustomerdetail(string FullName)
+        {
+            var customerdetails = _collection.Find(m => m.FullName == FullName).FirstOrDefault();
+
+            return customerdetails;
+        }
+
+        public void Create(CustomerEntity customerData)
+        {
+            _collection.InsertOne(customerData);
+        }
+
+        public void Update(string Id, CustomerEntity customerData)
+        {
+            var filter = Builders<CustomerEntity>.Filter.Eq(c => c.Id, Id);
+            var update = Builders<CustomerEntity>.Update
+                .Set("FullName", customerData.FullName)
+                .Set("Phonenumber ", customerData.Phonenumber)
+
+                .Set("Dateofevent", customerData.Dateofevent)
+
+                .Set("Eventlocation", customerData.Eventlocation)
+                .Set("Typeofevent", customerData.Typeofevent)
+                .Set("Numberofguest", customerData.Numberofguest);
+                
+            _collection.UpdateOne(filter, update);
+        }
+
+
+
+
+        public void Delete(string FullName)
+        {
+            var filter = Builders<CustomerEntity>.Filter.Eq(c => c.FullName, FullName);
+
+            _collection.DeleteOne(filter);
+        }
+
+
+
+
+
+    }
+
+}
